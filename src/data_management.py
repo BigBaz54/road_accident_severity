6ef1b334d3c6541976227baf3bb46490d1150259
BigBaz <louisvincent@famillecapelli.fr>
2024-01-10 15:21:59 -0500

    [+] more processing and data availability measurement

--- src/data_management.py
+++ src/data_management.py
@@ -7,7 +7,7 @@ def load_raw_data(path):
     :param path: path to the csv file
     :return: a pandas dataframe
     """
-    return pd.read_csv(path, sep=';', low_memory=False)
+    return pd.read_csv(path, sep=';', decimal=',', low_memory=False)
 
 def joined_data(caracteristiques, lieux, usagers, vehicules):
     """
@@ -32,20 +32,53 @@ def process_data(df):
     """
     Process the data :
     - replace birth year by age
-    - replace 'secu1', 'secu2', 'secu3' by 'secu' containing the number of equipments used
+    - add column 'secu' containing the number of equipments used + 1
+    - add column 'obscar' merging 'obs' and 'obsm' ('obms' if existing, else 'obs' + 10)
+    - replace letter values by numbers in 'actp'
+    - replace the one '#ERREUR' value in 'nbv' by -1
+    - add 1 to 'infra' to have 'no infrastructure' counting as an actual information
+    - add 1 to 'choc' to have 'no contact' counting as an actual information
+    - add 1 to 'nbv' to have '0 route' counting as an actual information
+    - add 1 to 'vosp' to have 'no reserved route' counting as an actual information
 
     :param df: a pandas dataframe
     :return: a pandas dataframe
     """
     df = df.copy()
+
     df['age'] = 2022 - df['an_nais']
     # Replace 'nan' values by -1
     df['age'].fillna(-1, inplace=True)
+
+    # Replace letter values by numbers in 'actp'
+    df['actp'] = df['actp'].apply(lambda x: 10 if x == 'A' else -1 if (x == 'B' or x == '-1' or x == ' -1') else int(x))
+
+    # Replace the one '#ERREUR' value in 'plan' by -1
+    df['nbv'] = df['nbv'].apply(lambda x: -1 if (x == '#ERREUR' or x == ' -1') else x)
+
+    # Add 1 to 'infra' to have 'no infrastructure' counting as an actual information
+    df['infra'] += 1
+
+    # Add 1 to 'choc'
+    df['choc'] += 1
+
+    # Add 1 to 'nbv'
+    df['nbv'].apply(lambda x: -1 if x == ' -1' else int(x) + 1 if int(x) > 0 else int(x))
+
+    # Add 1 to 'vosp'
+    df['vosp'] += 1
+
     
-    # Create column 'secu' containing the number of equipments used
-    df['secu'] = df['secu1'].apply(lambda x: 0 if x == 0 or x == -1 else 1)
+    # Create column 'secu' containing the number of equipments used + 1
+    df['secu'] = 1
+    df['secu'] += df['secu1'].apply(lambda x: 0 if x == 0 or x == -1 else 1)
     df['secu'] += df['secu2'].apply(lambda x: 0 if x == 0 or x == -1 else 1)
     df['secu'] += df['secu3'].apply(lambda x: 0 if x == 0 or x == -1 else 1)
+
+    # Create column 'obscar' merging 'obs' and 'obsm' ('obms' if existing, else 'obs' + 9)
+    df['obscar'] = df['obsm'].apply(lambda x: x + 1 if x > 0 else float('nan'))
+    df['obscar'].fillna(df['obs'].apply(lambda x: 1 if x == 0 else x + 10 if x > 0 else -1), inplace=True)
+
 
     df.drop(['an_nais'], axis=1, inplace=True)
 
@@ -66,7 +99,7 @@ def remove_missing_values(df):
     :param df: a pandas dataframe
     :return: a pandas dataframe
     """
-    values_to_remove = ['nan', '', -1, '.']
+    values_to_remove = [float('nan'), '', -1, '.', 0]
 
     df = df.copy()
 
@@ -74,6 +107,15 @@ def remove_missing_values(df):
         df = df[~df[col].isin(values_to_remove)]
     
     return df
+
+def data_available(df, feature):
+    """
+    Return the percentage of data available for a feature
+    :param df: a pandas dataframe
+    :param feature: a feature
+    :return: a float
+    """
+    return 100 * (1 - df[feature].isin([float('nan'), '', -1, '.', 0]).sum() / len(df))
 
 
 if __name__ == "__main__":
